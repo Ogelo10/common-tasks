c1d734a0f4095400b59e0d5b7f93030be6dbb687
Augustine Ofoegbu <augustineofoegbu1@gmail.com>
2023-01-05 17:26:26 -0800

    Update AsyncAwait_useEffect.jsx
    Added spin loader and Backdrop

--- AsyncAwait_useEffect.jsx
+++ AsyncAwait_useEffect.jsx
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useRef, useState } from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
-const [data, setData] = useState([])
-// const [loading, setLoading] = useState(false)
-// console.log(data)
+const [data, setData] = useState(null)
+const [loading, setLoading] = useState(false)
 
   let effectRan = useRef(false)
 
 useEffect(() => {
   console.log("effect ran")
-  // setLoading(true);
+  setLoading(true);
 if(effectRan.current === false){
   const fetchUsers = async () => {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+    // const response = await fetch('https://jsonplaceholder.typicode.com/photos')
     const json = await response.json()
     console.log(json)
+    setData(json)
   }
   // const fetchUsers = async () => {
   //   let response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
@@ -40,9 +42,14 @@ return () => {
   return (
     <div className="App">
       {
-        data.title
-        // loading? (data.title) : ("Loading")
+        data? (data.title) : (<Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>)
       }
+
     </div>
   );
 }
